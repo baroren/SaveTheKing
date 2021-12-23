c501b24561ba1306fa660161381360e13dc7845a
Netanel Bondat <netanelbondar74@gmail.com>
2021-12-24 00:16:49 +0200

    added connection with GameController and Window. there is loop for events and handlePressedKey function

--- src/Window.cpp
+++ src/Window.cpp
@@ -1,12 +1,43 @@
 #include "Window.h"
 
-#include <string>
+#include <iostream>
 
-using std::string;
+using std::cout;
+using std::endl;
 
 Window::Window(const sf::Vector2u windowSize, const string title)
+	: m_window(sf::VideoMode(windowSize.x, windowSize.y), title)
 {
 	m_window.setSize(windowSize);
 
 	m_window.setTitle(title);
+
+}
+
+void Window::display()
+{
+
+	m_window.clear(sf::Color(34, 20, 26));
+	m_window.display();
+
+
+	if (sf::Event event; m_window.pollEvent(event))
+	{
+		switch (event.type)
+		{
+		case sf::Event::Closed:
+			m_window.close();
+			break;
+		}
+	}
+}
+
+void Window::close()
+{
+	m_window.close();
+}
+
+bool Window::isOpen() const
+{
+	return m_window.isOpen();
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,19 @@
 ﻿
 #include "GameController.h"
 
+//	delete in the end
+#include "Window.h"
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
 int main()
 {
-	
+
+	GameController gameController;
+
+	gameController.run();
+
 	return 0;
 }
