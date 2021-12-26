b15934710e954c6a4c5819f36c7b198efaddd7ab
bar oren <bar.oren10@gmail.com>
2021-12-26 19:46:05 +0200

    ######################################## V.0.0.1 added:  set location  get location  (both on Gameobject class)
     To Do(GameObject):
     -add move func
     -fix graphics
     -do static objects (i dont think that there is a diffrent but i will look into it)
    
    ####################
     To Do (netanel):
     -fix game controller state manger (will work on it toget
    call me )
     -make board (for the שלד)
    
     ###############
     notes:
     working GameObject class

--- src/GameController.cpp
+++ src/GameController.cpp
@@ -22,6 +22,14 @@ void GameController::run()
 	while (window.isOpen())
 	{
 
+        sf::Event event;
+
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+        window.clear(sf::Color(34,20,26));
 
 
 
