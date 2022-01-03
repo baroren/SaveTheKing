3c8747694f03ccf476acdcc3889f411044c3352b
bar oren <bar.oren10@gmail.com>
2022-01-03 14:27:16 +0200

    ######################################## V.0.0.1 added:  set location  get location  (both on Gameobject class)
     To Do(GameObject):
     -add move func
     -fix graphics
     -do static objects (i dont think that there is a diffrent but i will look into it)
    
    ####################
     To Do (netanel):
     -fix game controller state manger (will work on it toget
    call me )
     -make board (for the שלד)
    
     ###############
     notes:
     working GameObject class

---
+++ cmake-build-debug/CMakeFiles/ex02.dir/cmake_clean.cmake
@@ -0,0 +1,37 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ex02.dir/src/AddTime.cpp.o"
+  "CMakeFiles/ex02.dir/src/Animation.cpp.o"
+  "CMakeFiles/ex02.dir/src/Board.cpp.o"
+  "CMakeFiles/ex02.dir/src/Button.cpp.o"
+  "CMakeFiles/ex02.dir/src/DestDwarf.cpp.o"
+  "CMakeFiles/ex02.dir/src/Dwarf.cpp.o"
+  "CMakeFiles/ex02.dir/src/Fire.cpp.o"
+  "CMakeFiles/ex02.dir/src/Game.cpp.o"
+  "CMakeFiles/ex02.dir/src/GameController.cpp.o"
+  "CMakeFiles/ex02.dir/src/GameObject.cpp.o"
+  "CMakeFiles/ex02.dir/src/Gate.cpp.o"
+  "CMakeFiles/ex02.dir/src/Gift.cpp.o"
+  "CMakeFiles/ex02.dir/src/Key.cpp.o"
+  "CMakeFiles/ex02.dir/src/King.cpp.o"
+  "CMakeFiles/ex02.dir/src/Mage.cpp.o"
+  "CMakeFiles/ex02.dir/src/MainMenu.cpp.o"
+  "CMakeFiles/ex02.dir/src/Menu.cpp.o"
+  "CMakeFiles/ex02.dir/src/Moving.cpp.o"
+  "CMakeFiles/ex02.dir/src/Orc.cpp.o"
+  "CMakeFiles/ex02.dir/src/Player.cpp.o"
+  "CMakeFiles/ex02.dir/src/Static.cpp.o"
+  "CMakeFiles/ex02.dir/src/SubTime.cpp.o"
+  "CMakeFiles/ex02.dir/src/Thief.cpp.o"
+  "CMakeFiles/ex02.dir/src/Throne.cpp.o"
+  "CMakeFiles/ex02.dir/src/Wall.cpp.o"
+  "CMakeFiles/ex02.dir/src/Warrior.cpp.o"
+  "CMakeFiles/ex02.dir/src/Window.cpp.o"
+  "CMakeFiles/ex02.dir/src/main.cpp.o"
+  "ex02"
+  "ex02.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ex02.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
