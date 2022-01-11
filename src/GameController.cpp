30e529a3210ebbf7d38e58466d0831239cbd0322
bar oren <bar.oren10@gmail.com>
2022-01-11 20:39:02 +0200

    addded textures images

--- src/GameController.cpp
+++ src/GameController.cpp
@@ -10,9 +10,9 @@ using std::endl;
 
 GameController::GameController()
 {
-    m_players.push_back(make_unique<Mage> (m_window.convertIndexToPixel(1, 1), 1, 3, 0.17, 3, "Mage.png", false));
+    m_players.push_back(make_unique<Mage> (m_window.convertIndexToPixel(1, 1), 1, 3, 0.17, 3, "Dwarf.png", true));
 
-    m_staticObjects.push_back(make_unique<Wall>(m_window.convertIndexToPixel(1, 2), 1, 4, 0.17, 3, "skeleton2_v2.png"));
+    m_staticObjects.push_back(make_unique<Wall>(m_window.convertIndexToPixel(1, 2), 1, 1, 0.17, 3, "Gate.png"));
 }
 
 
--- src/GameObject.cpp
+++ src/GameObject.cpp
@@ -22,8 +22,7 @@ GameObject::GameObject(const const sf::Vector2f position,
 
 
 
-    m_sprite.setOrigin(m_sprite.getGlobalBounds().width/(numOfFrames) /2.f,m_sprite.getGlobalBounds().height / (numOfFrames) /2.f+
-    m_sprite.getGlobalBounds().height / 2.f);
+    m_sprite.setOrigin(m_sprite.getGlobalBounds().width/(numOfFrames) /2.f,m_sprite.getGlobalBounds().height / (numOfAnim) /2.f);
 
     m_sprite.setPosition(position);
 
